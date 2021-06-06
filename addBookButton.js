e40dcf4787e05369b17b54994d8e980d4f3510b6
Fraanlol <franco.catas@hotmail.com>
2021-06-06 16:59:29 -0300

    Modified to match with booksOptions file

--- addBookButton.js
+++ addBookButton.js
@@ -1,24 +1,16 @@
-let button = document.querySelectorAll('.addImg');
 let form = document.querySelector('.addBook');
+let closeButton = document.querySelector('.closeAddButton');
+let button = document.querySelectorAll('.addImg');
 
 button.forEach(key => {
     key.addEventListener('click', () => {
-
         form.classList.toggle('addBookShow');
-        key.style.visibility = 'hidden'
-        menuButton.style.visibility="hidden";
-        
-    })
+        toggleButtons();
+    })  
 })
-
-
-let closeButton = document.querySelector('.closeAddButton');
 
 closeButton.addEventListener('click', () => {
     form.classList.toggle('addBookShow');
-    button.forEach(key => {
-        key.style.visibility = 'visible'
-    })
-    menuButton.style.visibility="visible";
+    toggleButtons();
 })
 
